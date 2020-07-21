4c69c9454184a7b829acaa7cef1b0a5d251e0e0e
Goi Jia Jian <jiajiannn@gmail.com>
2020-07-21 11:18:07 +0800

    feat: cert

--- src/templates/customTemplate/customTemplate.tsx
+++ src/templates/customTemplate/customTemplate.tsx
@@ -4,7 +4,6 @@ import { css } from "@emotion/core";
 import { GovTechCertificateTemplate } from "../sample";
 import certificateBase from "../../core/certificate-base.png";
 
-
 const container = css`
   font-family: "Lucida Sans Unicode", "Lucida Grande", sans-serif;
   text-align: center;
@@ -18,15 +17,51 @@ const container = css`
   position: fixed;
   top: 0;
   left: 0;
+  color: #525254;
 `;
 
 const name = css`
-  font-size: 48px;
-  // font-style: italic;
-  top: 350px;
+  font-size: 38px;
+  font-style: italic;
+  top: 340px;
   left: 90px;
   position: relative;
   // make it anchor at specific position
+  margin-bottom: 15px;
+`;
+
+const programmeName = css`
+  font-size: 20px;
+  // font-style: italic;
+  top: 340px;
+  left: 90px;
+  position: relative;
+  margin-bottom: 15px;
+`;
+
+const dateRange = css`
+  font-size: 16px;
+  // font-style: italic;
+  top: 340px;
+  left: 90px;
+  position: relative;
+  margin-bottom: 25px;
+`;
+
+const signature = css`
+  font-size: 16px;
+  // font-style: italic;
+  top: 340px;
+  left: 90px;
+  position: relative;
+`;
+
+const signatory = css`
+  font-size: 16px;
+  // font-style: italic;
+  top: 340px;
+  left: 90px;
+  position: relative;
 `;
 
 export const CustomTemplate: FunctionComponent<TemplateProps<GovTechCertificateTemplate> & { className?: string }> = ({
@@ -35,7 +70,16 @@ export const CustomTemplate: FunctionComponent<TemplateProps<GovTechCertificateT
 }) => {
   return (
     <div css={container} className={className} id="custom-template">
-      <div css={name}>{document.name}</div>
+      <div css={name}>{document.recipient.name}</div>
+      <div css={programmeName}>You have completed the {document.programme.name}.</div>
+      <div css={dateRange}>
+        {document.programme.startDate} to {document.programme.endDate}
+      </div>
+      <div css={signature}>
+        <img src={document.additionalData?.certSignatories[0].signature} />
+      </div>
+      <div css={signatory}>{document.additionalData.certSignatories[0].name}</div>
+      <div css={signatory}>{document.additionalData.certSignatories[0].position}</div>
     </div>
   );
 };
