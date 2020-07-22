94b77f16e23fe498cd6dd49d89f0bbb7031ac77e
Goi Jia Jian <jiajiannn@gmail.com>
2020-07-22 13:39:34 +0800

    fix: remove .webp

--- src/@types/declaration.d.ts
+++ src/@types/declaration.d.ts
@@ -10,10 +10,6 @@ declare module "*.svg" {
   const value: any;
   export default value;
 }
-declare module "*.webp" {
-  const value: any;
-  export default value;
-}
 
 declare module "@percy/testcafe" {
   const value: any;
