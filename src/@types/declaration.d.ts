6b49c860acd62c9e2c317a6d9dc0516e34983137
Goi Jia Jian <jiajiannn@gmail.com>
2020-07-22 13:31:01 +0800

    fix: update .webp type declaration

--- src/@types/declaration.d.ts
+++ src/@types/declaration.d.ts
@@ -10,6 +10,10 @@ declare module "*.svg" {
   const value: any;
   export default value;
 }
+declare module "*.webp" {
+  const value: any;
+  export default value;
+}
 
 declare module "@percy/testcafe" {
   const value: any;
