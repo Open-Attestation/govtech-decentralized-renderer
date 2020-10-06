aadf722caefd01d21ff8f0f160d8a09944fe2187
Goi Jia Jian <jiajiannn@gmail.com>
2020-10-06 12:36:54 +0800

    fix: integration

--- src/integration/integration.spec.ts
+++ src/integration/integration.spec.ts
@@ -5,9 +5,9 @@ import percySnapshot from "@percy/testcafe";
 const document = { ...sampleCertificate };
 fixture("GovTech Internship Certificate of Achievement").page`http://localhost:3000`;
 
-const CustomTemplate = Selector("#custom-template");
+const CustomTemplate = Selector("#certificate-of-achievement");
 
-test("Custom certificate is rendered correctly", async test => {
+test("Certificate of Achievement is rendered correctly", async test => {
   await test.eval(
     () => {
       // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
@@ -26,7 +26,13 @@ test("Custom certificate is rendered correctly", async test => {
     }
   );
   await test.expect(CustomTemplate.visible).ok();
-  await test.expect(CustomTemplate.textContent).contains("Certificate of Completion");
+  await test.expect(CustomTemplate.textContent).contains("Certificate of Achievement");
+  await test.expect(CustomTemplate.textContent).contains("Achievement unlocked! Way to go!");
+  await test.expect(CustomTemplate.textContent).contains("Jia Jian Goi");
+  await test.expect(CustomTemplate.textContent).contains("You have completed the GovTech Internship Programme.");
+  await test.expect(CustomTemplate.textContent).contains("4 May 2020 to 7 Oct 2020");
+  await test.expect(CustomTemplate.textContent).contains("Evangeline Chua");
+  await test.expect(CustomTemplate.textContent).contains("Chief People Officer");
 
   // take a snapshot
   await percySnapshot(test, "Rendered custom template");
