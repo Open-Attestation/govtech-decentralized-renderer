8b44f609c9299d4612d7f98230982d9d8cf39b84
Goi Jia Jian <jiajiannn@gmail.com>
2020-10-07 13:14:40 +0800

    fix: template type
    - removed the use of any, see https://github.com/Open-Attestation/decentralized-renderer-react-template/commit/ab744114a9c85a2776aa00ead03644508a602468

--- src/templates/index.tsx
+++ src/templates/index.tsx
@@ -1,6 +1,7 @@
 import { TemplateRegistry } from "@govtechsg/decentralized-renderer-react-components";
 import { templates } from "./certificateOfAchievement";
+import { GovTechCertificateTemplate } from "./sample";
 
-export const registry: TemplateRegistry<any> = {
+export const registry: TemplateRegistry<GovTechCertificateTemplate> = {
   CERTIFICATE_OF_ACHIEVEMENT: templates
 };
