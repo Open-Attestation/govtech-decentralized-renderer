22044e60502f822f50539721bdb6e159c2bdfae3
Matthea Loo <dhemutton@users.noreply.github.com>
2020-10-13 14:25:37 +0800

    feat: geekout template

---
+++ src/templates/geekOut2020/__tests__/certificateOfAchievement.test.tsx
@@ -0,0 +1,89 @@
+import { CertificateOfAchievement } from "../geekOut";
+import { sampleCertificate } from "../../sample";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import React from "react";
+
+describe("customTemplate", () => {
+  it("should render the certificate provided by the document", () => {
+    const { getByText } = render(
+      <CertificateOfAchievement document={sampleCertificate} handleObfuscation={() => void 0} />
+    );
+    expect(getByText("Certificate")).toBeInTheDocument();
+    expect(getByText("of")).toBeInTheDocument();
+    expect(getByText("Achievement")).toBeInTheDocument();
+  });
+  it("should render with recipient name provided by the document", () => {
+    const { getByText } = render(
+      <CertificateOfAchievement document={sampleCertificate} handleObfuscation={() => void 0} />
+    );
+    expect(getByText("Jia Jian Goi")).toBeInTheDocument();
+  });
+  it("should render with programme name provided by the document", () => {
+    const { getByText } = render(
+      <CertificateOfAchievement document={sampleCertificate} handleObfuscation={() => void 0} />
+    );
+    expect(getByText("You have completed the GovTech Internship Programme.")).toBeInTheDocument();
+  });
+  it("should render with the start and end date provided by the document (format 1: mm-ddd-yyyy)", () => {
+    const { getByText } = render(
+      <CertificateOfAchievement document={sampleCertificate} handleObfuscation={() => void 0} />
+    );
+    expect(getByText("4 May 2020 to 7 Oct 2020")).toBeInTheDocument();
+  });
+  it("should render with the start and end date provided by the document (format 2: mm ddd yy)", () => {
+    const { getByText } = render(
+      <CertificateOfAchievement
+        document={{
+          ...sampleCertificate,
+          programme: {
+            ...sampleCertificate.programme,
+            startDate: "04 May 20",
+            endDate: "07 Oct 20"
+          }
+        }}
+        handleObfuscation={() => void 0}
+      />
+    );
+    expect(getByText("4 May 2020 to 7 Oct 2020")).toBeInTheDocument();
+  });
+  it("should render with the start and end date provided by the document (format 3: mm/ddd/yy)", () => {
+    const { getByText } = render(
+      <CertificateOfAchievement
+        document={{
+          ...sampleCertificate,
+          programme: {
+            ...sampleCertificate.programme,
+            startDate: "04/May/20",
+            endDate: "07/Oct/20"
+          }
+        }}
+        handleObfuscation={() => void 0}
+      />
+    );
+    expect(getByText("4 May 2020 to 7 Oct 2020")).toBeInTheDocument();
+  });
+  it("should render with the start and end date provided by the document (format 4: ISO-8601)", () => {
+    const { getByText } = render(
+      <CertificateOfAchievement
+        document={{
+          ...sampleCertificate,
+          programme: {
+            ...sampleCertificate.programme,
+            startDate: "2020-05-04",
+            endDate: "2020-10-07"
+          }
+        }}
+        handleObfuscation={() => void 0}
+      />
+    );
+    expect(getByText("4 May 2020 to 7 Oct 2020")).toBeInTheDocument();
+  });
+  it("should render with the signatory details provided by the document", () => {
+    const { getByText } = render(
+      <CertificateOfAchievement document={sampleCertificate} handleObfuscation={() => void 0} />
+    );
+    expect(getByText("Evangeline Chua")).toBeInTheDocument();
+    expect(getByText("Chief People Officer")).toBeInTheDocument();
+  });
+});
---
+++ src/templates/geekOut2020/index.tsx
@@ -0,0 +1,9 @@
+import { GeekOut2020 } from "./geekOut2020";
+
+export const geekOut2020Template = [
+  {
+    id: "GEEK_OUT_2020",
+    label: "Certificate",
+    template: GeekOut2020
+  }
+];
