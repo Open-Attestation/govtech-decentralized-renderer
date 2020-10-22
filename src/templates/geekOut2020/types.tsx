84e9d3a2a6c8a3d7d414bc3ca6277aadc6d063be
Matthea Loo <dhemutton@users.noreply.github.com>
2020-10-22 10:39:52 +0800

    fix: type fix

--- src/templates/geekOut2020/types.tsx
+++ src/templates/geekOut2020/types.tsx
@@ -1,8 +1,4 @@
 import { v2 } from "@govtechsg/decentralized-renderer-react-components";
 import { geekout } from "@govtechsg/oa-schemata";
 
-export interface GeekOutCertificateTemplate extends v2.OpenAttestationDocument, geekout.Geekout {
-  recipient: {
-    name: string;
-  };
-}
+export type GeekOutCertificateTemplate = v2.OpenAttestationDocument & geekout.Geekout;
