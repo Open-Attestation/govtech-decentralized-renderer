f196e83215b3c9ff9e0a37b00ab8a7cb3976277d
Matthea Loo <dhemutton@users.noreply.github.com>
2020-11-19 14:55:45 +0800

    fix: imports for sample fix

--- application/index.tsx
+++ application/index.tsx
@@ -1,23 +1,14 @@
 import ReactDOM from "react-dom";
-import { sampleCertificate } from "../src/templates/sample";
+import { sampleCertificate as certificateOfAchievement } from "../src/templates/certificateOfAchievement/fixtures/sample";
+import { sampleCertificate as geekOut2020 } from "../src/templates/geekOut2020/fixtures/sample";
 import React from "react";
 import { App } from "./app";
 
 ReactDOM.render(
   <App
     documents={[
-      { name: "Default document", document: sampleCertificate },
-      {
-        name: "Red document",
-        document: {
-          ...sampleCertificate,
-          foo: "bar",
-          $template: {
-            ...sampleCertificate.$template,
-            name: "red"
-          }
-        }
-      }
+      { name: "GovTech document - Certificate of Achievement", document: certificateOfAchievement },
+      { name: "GovTech document - GeekOut 2020", document: geekOut2020 }
     ]}
   />,
   document.getElementById("root")
