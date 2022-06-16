edc26970aff0873bb9d02ec4fb41c6057246ae28
Lucas <31716292+yapyuyou@users.noreply.github.com>
2022-06-16 16:47:30 +0800

    feat: initial commit

--- src/templates/index.tsx
+++ src/templates/index.tsx
@@ -3,10 +3,12 @@ import { templates } from "./certificateOfAchievement";
 import { geekOut2020Template } from "./geekOut2020";
 import { geekOut2021Template } from "./geekOut2021";
 import { covidLetterOfAppreciationTemplate } from "./covidAppreciationLetter";
+import { geekOutV2Template } from "./geekOutV2";
 
 export const registry: TemplateRegistry<any> = {
   CERTIFICATE_OF_ACHIEVEMENT: templates,
   GEEK_OUT_2020: geekOut2020Template,
   GEEK_OUT_2021: geekOut2021Template,
+  GEEK_OUT_V2: geekOutV2Template,
   COVID_LETTER_OF_APPRECIATION_2021: covidLetterOfAppreciationTemplate
 };
