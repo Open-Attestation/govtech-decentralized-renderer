a62641b149d918384537d68234bed97c8d7f4dfc
Lucas <lucas_yap@tech.gov.sg>
2023-06-23 14:37:50 +0800

    chore: update types.tsx

--- src/templates/geekOut/geekOutV2/types.tsx
+++ src/templates/geekOut/geekOutV2/types.tsx
@@ -1,7 +1,7 @@
 import { v2 } from "@govtechsg/decentralized-renderer-react-components";
 import { geekout } from "@govtechsg/oa-schemata";
 
-export type DocumentType = "champion" | "runner-up" | "completion";
+export type DocumentType = "champion" | "runner-up" | "completion" | "first" | "second" | "third" | "special-mention";
 
 export type GeekOutCertificateTemplateV2 = v2.OpenAttestationDocument &
   geekout.Geekout & {
